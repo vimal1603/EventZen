b19d8e4ca623eb96f9e57f70843b0f560ca1fd2b
vimal1603 <vimalc162020@gmail.com>
2024-06-27 18:46:40 +0530

    Change

--- src/Birthday/BdaySlide.js
+++ src/Birthday/BdaySlide.js
@@ -18,7 +18,7 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: 'center' ,
   color: theme.palette.text.secondary,
 }));
 
